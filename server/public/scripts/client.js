df5ad0364ffa4cd5c8b7c999f661e98d28381317
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-08-08 13:45:32 -0500

    done

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,12 @@
-console.log('js');
+let FoodApp = angular.module( 'FoodApp', [] );
+
+FoodApp.controller( 'FoodController', function(){
+    let vm = this;
+    vm.myFood = [];
+
+    vm.addFood = function(){
+        console.log('in addFood', vm.foodIn);
+        vm.myFood.push(vm.foodIn);
+        console.log('my food:', vm.myFood);
+    }
+});
